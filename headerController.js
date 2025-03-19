9db5c8a0a4f2635f8bb8e8599114869a91b40cbb
MatiCarini <matiascarini@gmail.com>
2025-03-19 15:10:43 -0300

    Mobile responsive/header.Scrollbehaviour solucion

--- headerController.js
+++ headerController.js
@@ -1,11 +1,21 @@
-const header = document.getElementById("main-header");
+const getScrollPosition = () => {
+  return document.documentElement.scrollTop || document.body.scrollTop || 0;
+};
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 75) {
-    header.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
-  }
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(document.documentElement.scrollTop, document.body.scrollTop);
+  document.addEventListener("scroll", () => console.log("Scrolling..."), true);
+  window.addEventListener("scroll", () => {
+    const header = document.getElementById("main-header");
+    let scrollTop = getScrollPosition();
+    console.log(scrollTop); // Verifica si esto se actualiza en móvil
+
+    if (scrollTop > 75) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
 });
 
 const hamburguerMenu = document.getElementById("hamburger-menu");
